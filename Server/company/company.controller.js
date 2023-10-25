c973a2a9561ffcdb70095c3c06b7cb70622e3d6e
Sitra Mohammed <117744439+sitt-a@users.noreply.github.com>
2023-10-25 09:00:52 +0300

    Add files via upload

---
+++ Server/company/company.controller.js
@@ -0,0 +1,46 @@
+const {createCompany , loginCompany, getPostedJobs} = require("./company.model") 
+async function httpCreateCompany(req, res){
+    const body = req.body
+    const returned  = await createCompany(body);
+    console.log(body)
+    console.log("create request")
+    if(returned === true){
+        console.log("working")
+        res.status(201).json({
+            ok : returned
+
+        })
+    } else{
+        res.status(400).json({
+            message : returned
+        })
+    }
+}
+
+async function httpLoginCompany(req, res){
+    return await loginCompany(req, res)
+}
+
+
+async function httpGetPostedJobs(req, res){
+    companyId = res.locals.companyId
+    result = await getPostedJobs(companyId)
+
+   
+    if(result){
+      
+  
+
+      
+        res.status(200).json(result)
+    }else{
+        res.status(400).json({ok : false})
+    }
+}
+
+
+module.exports = {
+    httpGetPostedJobs,
+    httpCreateCompany ,
+     httpLoginCompany , 
+    }
